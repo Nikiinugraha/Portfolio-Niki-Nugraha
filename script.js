0486f7be2b75142597242aeeb1d1793e7d018455
Nikiinugraha <162570479+Nikiinugraha@users.noreply.github.com>
2024-10-21 09:06:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const hamburger_icon = document.getElementById("hamburger-icon");
+const menuList = document.getElementById("menu-list");
+
+hamburger_icon.addEventListener("click", () => {
+    menuList.classList.toggle("hidden");
+});
